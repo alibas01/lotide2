3910616608486abc91a774039c92c84d1391a339
Ali Bas <alibas01@gmail.com>
2021-01-11 18:59:58 -0500

    a

--- test/headTest.js
+++ test/headTest.js
@@ -10,8 +10,8 @@ describe("#head", () => {
   it("returns '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5'); 
   });
+  it("returns 'Hello' for array", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+  });
 });
 
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// console.log(head([]));
